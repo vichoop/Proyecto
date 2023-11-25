ab70c980d079913d86f3d464bd889614b9c95b03
vichoop <150629326+vichoop@users.noreply.github.com>
2023-11-24 23:45:36 -0300

    Comentarios.ino

--- Grupo Control_Excel.ino
+++ Grupo Control_Excel.ino
@@ -4,35 +4,45 @@
 #include "Adafruit_CCS811.h" // Librería CCS811 (Sensor de Dióxido de Carbono)
 #include <LiquidCrystal.h> // Librería LCD
 
-#define DHTPIN 10 // El pin digital dónde está conectado el sensor DHT11 es el 2. 
+#define DHTPIN 10 // Definición del Pin Digital del sensor DHT11.
 #define DHTTYPE DHT11 // La librería de DHT está creada también para su sensor hermano (DHT22), entonces hay que especificar cuál es.
-#define SensorPin A0
+#define SensorPin A0 // Definición del Pin Análogo del sensor de Humedad del Suelo.
 
-DHT dht(DHTPIN, DHTTYPE);
-LiquidCrystal lcd(8, 9, 4, 5, 6, 7); // Configura los pines de la pantalla LCD
-Adafruit_CCS811 ccs;
+DHT dht(DHTPIN, DHTTYPE); // Ejecución de la librería del DHT y nombre del sensor: 'dht'.
+LiquidCrystal lcd(8, 9, 4, 5, 6, 7); // Ejecución de la libraría del LCD y Configuración Pines Pantalla LCD (rs, en, d4, d5, d6, d7).
+Adafruit_CCS811 ccs; // Ejecución de la librería del CCS811 y nombre del sensor 'ccs'.
 
 void setup() {
   Serial.begin(9600);
-  Serial.println("CLEARDATA"); // Limpia datos -- Este comando borra solo los datos registrados
-  Serial.println("CLEARSHEET"); //Limpia hoja -- Este comando borra todos los datos de ActiveSheet (¡incluidas las etiquetas!). 
-  Serial.println("LABEL,Fecha,Hora,Temperatura,Humedad Ambiental,Humedad Relativa,Dióxido de Carbono");
+
+  // Inicio Integración Excel
+  
+  Serial.println("CLEARDATA"); 
+  Serial.println("CLEARSHEET"); 
+  Serial.println("LABEL,Fecha,Hora,Temperatura,Humedad Ambiental,Humedad Relativa,Dióxido de Carbono"); // Creación Encabezados Hoja de Cálculo
   Serial.println("RESETTIMER");
-  lcd.begin(16, 2); // Inicializa la pantalla LCD con 16 columnas y 2 filas
-  dht.begin();
-  ccs.begin();
+
+  // Fin Integración Excel
+  
+  lcd.begin(16, 2); // Inicialización Pantalla LCD
+  dht.begin(); // Inicialización Sensor DHT11
+  ccs.begin(); // Inicialización Sensor CCS811
 }
 
 
 void loop() {
-  ccs.readData();
-  delay(1000);
+  ccs.readData(); // Orden al sensor CCS811 para que reciba datos.
+  
   float Temperatura = dht.readTemperature(); // Clasificación de Variable de Temperatura (en C°).
   float HA = dht.readHumidity(); // Clasificación de Variable de Humedad Ambiental (en %).
   int  HS=map(analogRead(SensorPin), 0, 1023, 100, 0); // Clasificación de Variable de Humedad del Suelo (en %).
   float co2 = ccs.geteCO2(); // Clasificación de Variable de Dióxido de Carbono (en ppm).
 
-  Serial.print("DATA,DATE,TIME,"); // Primero se escribe DATA, luego DATE significa la fecha del sistema, TIMER son los segundos desdel último envío,Es importante la última coma de esta línea
+  // Inicio Integración Excel
+
+  Serial.print("DATA,DATE,TIME,"); // Exportación de lo recopilado
+
+  // Fin Integración Excel
 
   // Actualiza la pantalla LCD con los datos
   lcd.clear();
